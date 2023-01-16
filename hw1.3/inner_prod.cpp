c7aba7d11c2c7dff0e2efda0271a2083a5c8671b
Tina <tina@Tinas-MacBook-Air.local>
2023-01-16 00:02:20 -0600

    Add solution for part 3

--- hw1.3/inner_prod.cpp
+++ hw1.3/inner_prod.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
 #include<numeric>
 #include<vector>
+#include<cmath>
+
 using namespace std;
 int main(){
-    vector<int> v;
+    vector<double> v;
     for(int i =1; i<=6; i++){
         v.push_back((i));
     }
     cout << "Using inner product\n";
-    cout << "Sum of Squares of all elemets in array  : " << inner_product(v.begin(), v.end(), v.begin(), 0) << endl;
+    double sq_sum = inner_product(v.begin(), v.end(), v.begin(), 0);
+    cout << "Sum of Squares of all elemets in array  : " << sq_sum << endl;
+    cout << " Euclidean Distance: " <<sqrt(sq_sum)<<"\n";
 }
